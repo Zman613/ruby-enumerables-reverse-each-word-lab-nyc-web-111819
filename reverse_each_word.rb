9efea8cf2bcd3537f2407819e4bedc18cd15406f
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-14 23:42:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,7 @@ def reverse_each_word(str)
   array << str
   %w[array]
   pp array
-  new_array = []
   array.each do |string|
-    new_array << "#{string.reverse!}"
-    puts "#{new_array}"
+    puts << "#{string.reverse}"
   end
 end
